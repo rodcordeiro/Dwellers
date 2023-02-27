16b0b949a951c81092d781d4b9b5a1dd3758c5f7
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-02-26 22:00:24 -0300

    Chore: adding test setup

--- src/database.connection.ts
+++ src/database.connection.ts
@@ -5,9 +5,10 @@ import { Building } from './entities/Buildings';
 import { Job } from './entities/Jobs';
 import { Assignment } from './entities/Assignment';
 
+const db_name = process.env.NODE_ENV === 'test'?'test':'dwellers'
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: `${__dirname}/../dwellers.sqlite3`,
+  database: `${__dirname}/../${db_name}.sqlite3`,
   synchronize: true,
   logging: false,
   entities: [Dweller, Building, Job, Assignment],
